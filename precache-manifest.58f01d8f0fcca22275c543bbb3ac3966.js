31f6e65603e0f7ee2dd56586d91b831b24660048
Ian <41586905+ianbaxter@users.noreply.github.com>
2020-03-30 19:10:48 +0100

    Updates

---
+++ precache-manifest.58f01d8f0fcca22275c543bbb3ac3966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44826c09744712f146650e23871ff13e",
+    "url": "/boggler/index.html"
+  },
+  {
+    "revision": "9aef9f0cee26a0d090c1",
+    "url": "/boggler/static/css/main.18efe4be.chunk.css"
+  },
+  {
+    "revision": "e8162971f484c3d12884",
+    "url": "/boggler/static/js/2.bc4eb2f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boggler/static/js/2.bc4eb2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aef9f0cee26a0d090c1",
+    "url": "/boggler/static/js/main.8747c9dd.chunk.js"
+  },
+  {
+    "revision": "b3dd7e2389f7289243fc",
+    "url": "/boggler/static/js/runtime-main.f9b69a58.js"
+  }
+]);
